39e6ca237ffe3fb5bc6fd308877c12a7805044d1
Ho Hsuan-I <azuxmioy@gmail.com>
2017-12-21 00:40:24 +0800

    change

--- js/dialog.js
+++ js/dialog.js
@@ -1,127 +1,20 @@
-// JS for demo
-$( document ).ready(function() {
-$( "#dialog1" ).dialog({
-			autoOpen: false,
-			show: {
-				effect: "fade",
-				duration: 150
-			},
-			hide: {
-				effect: "fade",
-				duration: 150
-			},
-			position: {
-  			my: "center",
-  			at: "center"
-			},
-			// Add the 2 options below to use click outside feature
-			clickOutside: true, // clicking outside the dialog will close it
-			clickOutsideTrigger: "#opener1"  // Element (id or class) that triggers the dialog opening 
-		});
-		
-    $( "#dialog2" ).dialog({
-			autoOpen: false,
-			show: {
-				effect: "fade",
-				duration: 150
-			},
-			hide: {
-				effect: "fade",
-				duration: 150
-			},
-			position: {
-  			my: "center",
-  			at: "center"
-			},
-			// Add the 2 options below to use click outside feature
-			clickOutside: true, // clicking outside the dialog will close it
-			clickOutsideTrigger: "#opener2"  // Element (id or class) that triggers the dialog opening
-		});		
+// Get the modal
+var modal = document.getElementById('myModal');
 
-    $( "#dialog3" ).dialog({
-			autoOpen: false,
-			show: {
-				effect: "fade",
-				duration: 150
-			},
-			hide: {
-				effect: "fade",
-				duration: 150
-			},
-			position: {
-  			my: "center",
-  			at: "center"
-			},
-			clickOutside: false // For demo purpose. Not necessary because this is the default value
-		});	
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
 
-		$( "#opener1" ).click(function() {
-			$( "#dialog1" ).dialog( "open" );
-		});
-		
-		$( "#opener2" ).click(function() {
-			$( "#dialog2" ).dialog( "open" );
-		});
-		
-		$( "#opener3" ).click(function() {
-			$( "#dialog3" ).dialog( "open" );
-		});
-});
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-/* jQuery UI dialog clickoutside */
-
-/*
-The MIT License (MIT)
-
-Copyright (c) 2013 - AGENCE WEB COHERACTIO
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
-$.widget( "ui.dialog", $.ui.dialog, {
-  options: {
-    clickOutside: false, // Determine if clicking outside the dialog shall close it
-    clickOutsideTrigger: "" // Element (id or class) that triggers the dialog opening 
-  },
-
-  open: function() {
-    var clickOutsideTriggerEl = $( this.options.clickOutsideTrigger );
-    var that = this;
-    
-    if (this.options.clickOutside){
-      // Add document wide click handler for the current dialog namespace
-      $(document).on( "click.ui.dialogClickOutside" + that.eventNamespace, function(event){
-        if ( $(event.target).closest($(clickOutsideTriggerEl)).length == 0 && $(event.target).closest($(that.uiDialog)).length == 0){
-          that.close();
-        }
-      });
-    }
-    
-    this._super(); // Invoke parent open method
-  },
-  
-  close: function() {
-    var that = this;
-    
-    // Remove document wide click handler for the current dialog
-    $(document).off( "click.ui.dialogClickOutside" + that.eventNamespace );
-    
-    this._super(); // Invoke parent close method 
-  },  
-
-});
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
