d2e0a92ee08ab3c56e7912c9621648b15a0cc597
Ho Hsuan-I <azuxmioy@gmail.com>
2017-12-21 12:13:02 +0800

    change

--- js/youtube.js
+++ js/youtube.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
     $this.attr('src', tempSrc);
   });
 
-  $('#html5Video').on('hidden.bs.modal', function() {
-    var html5Video = document.getElementById("htmlVideo");
-    if (html5Video != null) {
-      html5Video.pause();
-      html5Video.currentTime = 0;
-    }
+  $('#youtubeVideo2').on('hidden.bs.modal', function() {
+    var $this = $(this).find('iframe'),
+      tempSrc = $this.attr('src');
+    $this.attr('src', "");
+    $this.attr('src', tempSrc);
   });
+
 });
